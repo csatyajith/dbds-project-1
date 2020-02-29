6a6dc309e7b83742e7f6c57a667134b25a0b8c51
csatyajith <csatyajith@gmail.com>
2020-02-28 21:26:41 -0500

    Code written to connect to s3 and push data from s3 to rds mysql

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # dbds-project-1
+
+<b>Configuration</b>
+
+* Always start off by creating a local_config.json file. This file should <b>NOT</b> be checked into version control.
+It will contain AWS secrets that I will give you and failing to protect those will cost us money.
+
+* A sample format for local_config.json can be found in the local_config_template.json. Replicate that file and replace
+the variables accordingly.
+
+<b>Description of the project:</b>
+
+* The feed_data.py file feeds the instacart data on AWS S3 to RDS MySql. It should ideally only be run once unless the
+data changes.
---
+++ feed_data.py
@@ -0,0 +1,35 @@
+import json
+
+import pandas as pd
+import smart_open
+import sqlalchemy
+
+with open("local_config.json", "r") as config_file:
+    config = json.load(config_file)
+
+
+def get_sql_alchemy_connection():
+    engine = sqlalchemy.create_engine(
+        "mysql+pymysql://{}:{}@{}/dbds_project_1".format(
+            config["rds_username"], config["rds_password"], config["rds_host"]))
+    return engine.connect()
+
+
+def load_instacart_to_sql(file_names, connection):
+    for file_name in file_names:
+        access_url = "s3://{}:{}@dbds-project/instacart/{}".format(config["aws_access_key_id"],
+                                                                   config["aws_secret_access_key"], file_name)
+        with smart_open.open(access_url, "r") as f:
+            df = pd.read_csv(f)
+
+        df.to_sql(file_name[:-4], connection, if_exists="replace")
+
+
+if __name__ == '__main__':
+    conn = get_sql_alchemy_connection()
+    files = ["aisles.csv", "products.csv", "orders.csv", "order_products.csv", "departments.csv"]
+    # load_instacart_to_sql(file_names, conn)
+    for file in files:
+        table_name = file[:-4]
+        for row in conn.execute("select count(*) from {}".format(table_name)):
+            print(row)
